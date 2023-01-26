083c01106ee3f734e75681f5e42f334c38df23dd
Dazn-des <76596785+zoppidavide@users.noreply.github.com>
2023-01-26 14:49:06 +0100

    26.01.23 update 2.1 Si vede la luce in fondo al tunnel

--- pages/negroni.vue
+++ pages/negroni.vue
@@ -26,38 +26,6 @@
         </div>
       </div>
 
-      <div class='justify-content-center d-lg-flex mt-3 mt-lg-5'>
-        <div class='d-flex col-10 alone'>
-          <img src='~/assets/Persona_4.png' class="img-fluid"/>
-        </div>
-      </div>
-
-      <div class='justify-content-center d-lg-flex mt-3 mt-lg-5'>
-        <div class='d-flex col-10'>
-          <img src='~/assets/Persona_3.png' class="img-fluid"/>
-        </div>
-      </div>
-
-      <div class='justify-content-center d-lg-flex mt-3 mt-lg-5'>
-        <div class='d-flex col-10 alone'>
-          <img src='~/assets/Persona_5.png' class="img-fluid"/>
-        </div>
-      </div>
-
-      <div class='justify-content-center d-lg-flex mt-3 mt-lg-5'>
-        <div class='d-flex col-10 alone'>
-          <img src='~/assets/Persona_6.png' class="img-fluid"/>
-        </div>
-      </div>
-
-      <div class='justify-content-center d-lg-flex mt-3 mt-lg-5'>
-        <div class='col-5'>
-          <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/792811784?h=7e79885c60&autoplay=1&loop=1&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-        </div>
-        <div class='col-5'>
-          <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/792813345?h=86140e8654&autoplay=1&loop=1&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-        </div>
-      </div>
 
       <div class='row mx-3 mt-5'>
 
